50e1ad16786fa2ab80cfe8bf8145e71d4bca68d5
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2023-03-01 20:54:32 -0300

    Refinamento no trabalho
    Ajustado erros no layout das msg enviadas pelo systema

---
+++ src/functions/send-message.js
@@ -0,0 +1,37 @@
+const arrayMsg = []
+const arrayMsgSystem = []
+const elements = []
+
+function sendMsg() {
+  const msg = document.getElementById('msg')
+  if (msg.value != '') {
+    let bodyChat = document.getElementById('chat')
+
+    elements.push(message(msg.value, 'USER'))
+    elements.push(message('Olá!', 'SYSTEM'))
+    elements.push(
+      message('No momento nosso sistema não está disponível', 'SYSTEM')
+    )
+
+    bodyChat.innerHTML = elements.join('')
+    msg.value = ''
+
+    bodyChat.lastChild.scrollIntoView({ behavior: 'smooth' })
+  }
+}
+
+function messageBaseUser(message) {
+  return `<div class="mymsg">
+    <div class="sent-msg msgMySent"><p><strong>Você</strong><br>${message}</p></div>
+  </div><br>`
+}
+
+function messageBaseSystem(message) {
+  return `<div class="msgSystem">
+    <div class="sent-msg msgSystemSent"><p><strong>Atendimento</strong><br>${message}</p></div>
+  </div><br>`
+}
+
+function message(message, type) {
+  return type == 'USER' ? messageBaseUser(message) : messageBaseSystem(message)
+}
