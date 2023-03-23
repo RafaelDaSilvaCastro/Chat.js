bc44e099102133548f24264e93e8eda39e6ac3e5
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2023-03-23 19:48:12 -0300

    Finalizando ajustes

--- src/functions/send-message.js
+++ src/functions/send-message.js
@@ -2,61 +2,45 @@
  * Nomes: Rafael da Silva Castro & Vito Loch Mafei
  *
  */
-
 function sendMsg() {
   const msg = document.getElementById('msg')
   if (msg.value != '') {
-    let bodyChat = document.getElementById('chat')
-
-    const MessageUser = document.createElement('div')
-    MessageUser.classList.add('mymsg')
-    const ContentMessageUser = document.createElement('div')
-    ContentMessageUser.classList.add('sent-msg')
-    ContentMessageUser.classList.add('msgMySent')
-
-    ContentMessageUser.innerHTML = newMessageBaaseUser(msg.value)
-    MessageUser.appendChild(ContentMessageUser)
-    bodyChat.appendChild(MessageUser)
-
-    let MessageBySystem = document.createElement('div')
-    MessageBySystem.classList.add('msgSystem')
-    let ContentMessageBySystem = document.createElement('div')
-    ContentMessageBySystem.classList.add('sent-msg')
-    ContentMessageBySystem.classList.add('msgSystemSent')
-
-    ContentMessageBySystem.innerHTML = newMessageBaseSystem('Olá!')
-    MessageBySystem.appendChild(ContentMessageBySystem)
-    bodyChat.appendChild(MessageBySystem)
-
-    MessageBySystem = document.createElement('div')
-    MessageBySystem.classList.add('msgSystem')
-
-    ContentMessageBySystem = document.createElement('div')
-    ContentMessageBySystem.classList.add('sent-msg')
-    ContentMessageBySystem.classList.add('msgSystemSent')
-    ContentMessageBySystem.innerHTML = newMessageBaseSystem(
-      'No momento nosso sistema não está disponível'
-    )
-
-    MessageBySystem.appendChild(ContentMessageBySystem)
-    bodyChat.appendChild(MessageBySystem)
-
-    msg.value = ''
-
-    bodyChat.lastChild.scrollIntoView({ behavior: 'smooth' })
+    createMessages(msg.value)
+    msg.value = ""
   }
 }
 
-function newMessageBaaseUser(message) {
-  return `<p><strong>Você</strong><br>${message}<br></p>`
+function newMessageUser(message) {
+  const MessageUser = document.createElement('div')
+  MessageUser.classList.add('mymsg')
+  const ContentMessageUser = document.createElement('div')
+  ContentMessageUser.classList.add('sent-msg')
+  ContentMessageUser.classList.add('msgMySent')
+  ContentMessageUser.innerHTML = `<p><strong>Você</strong><br>${message}<br></p>`
+  MessageUser.appendChild(ContentMessageUser)
+  return MessageUser
 }
 
 function newMessageBaseSystem(message) {
-  return `<p><strong>Atendimento</strong><br>${message}</p>`
+  let MessageBySystem = document.createElement('div')
+  MessageBySystem.classList.add('msgSystem')
+  let ContentMessageBySystem = document.createElement('div')
+  ContentMessageBySystem.classList.add('sent-msg')
+  ContentMessageBySystem.classList.add('msgSystemSent')
+
+  ContentMessageBySystem.innerHTML = `<p><strong>Atendimento</strong><br>${message}</p>`
+  MessageBySystem.appendChild(ContentMessageBySystem)
+  return MessageBySystem
 }
 
-function message(message, type) {
-  return type == 'USER'
-    ? newMessageBaaseUser(message)
-    : newMessageBaseSystem(message)
+function createMessages(messageOfUser){
+  let bodyChat = document.getElementById('chat')
+
+  const messageUser = newMessageUser(messageOfUser)
+  bodyChat.appendChild(messageUser)
+
+  const messageBySystem = newMessageBaseSystem('No momento nosso sistema não está disponível')
+  bodyChat.appendChild(messageBySystem)
+
+  bodyChat.lastChild.scrollIntoView({ behavior: 'smooth' })
 }
