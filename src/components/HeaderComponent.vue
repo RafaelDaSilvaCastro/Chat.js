5ffb3e04ee55cbc0cc693efa90385412ce32576f
Vitor Loch Mafei Rosa <vitorloch62@gamil.com>
2023-04-19 23:35:44 -0300

    FEAT: Add  Vue

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header>
+        <div id="textHeader">
+            <h3>Atendimento Online</h3>
+        </div>
+
+    </header>
+</template>
+
+<style scoped>
+header{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-top: 2rem;
+}
+
+#textHeader{
+    background-color: rgb(54, 50, 50);
+    color: aliceblue;
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px 5px 0px 0px;
+}
+h3{
+    margin: 0;
+    font-size: 1.5rem;
+}
+
+</style>
---
+++ src/interface/Message.ts
@@ -0,0 +1,4 @@
+export default interface IMessage {
+    value: string,
+    type: string
+}
